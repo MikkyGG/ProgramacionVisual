ec0bed46cb34a6c115290e52581f506684963c55
Miguel Palma <mikky@MacBook-Air-de-Miguel.local>
2019-10-01 10:50:56 -0500

    Apuntes de clase

--- hola_python.py
+++ hola_python.py
@@ -142,3 +142,132 @@ Use min() y max() para asegurarse de que su lista realmente comience en uno
 y termine en un millon.
 
 Ademas, use la funcion sum() para ver que tan rapido Python puede sumar un millon de numeros
+
+"""
+
+# MOVER VARIABLES
+
+cars = ["audi", "bmw", "subaru", "toyota"]
+
+for car in cars:
+	# print(car)
+	if car == "bmw":
+		print(car.upper())
+	else:
+		print(car.title())
+
+
+respuesta = 17
+
+if respuesta != 42:
+	print("Respuesta incorrecta")
+
+
+
+user_ban = ["pepe charly", "jose", "maria"]
+
+user = input("Ingresa tu nombre de usuario: ")
+
+if user not in user_ban:
+	print(user.title() + " no esta baneado")
+
+else:
+
+	print(user.title() + " esta baneado")
+
+
+
+"""
+if - elif - else
+
+La entrada para menores de 4 años es gratuita
+La entrada para cualquier persona entre las edades de 4 y18 años es de $ 50
+La entrada para cualquier persona mayor de 18 años es de $100
+"""
+
+operacion = input("Ingrese la operacion a realizar: ")
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo nuero: "))
+
+if operacion == "suma":
+	resultado = num1 + num2
+
+elif operacion == "resta":
+	resultado = num1 - num2
+
+elif operacion == "division":
+	resultado = num1 / num2
+
+else: 
+	resultado = num1 * num2
+
+print("El resultado de la operacion: " + str(operacion) + " es " + str(resultado))
+
+
+
+"""
+diccionarios
+
+"""
+participantes = {
+	
+"nombre": "Miguel",
+"edad": 22,
+"cursos": ["Python", "Django", "Pycharm"]
+
+}
+
+print(participantes["nombre"])
+print(participantes["edad"])
+print(participantes["cursos"])
+
+participantes["telefono"] = 9811126236
+participantes["ocupacion"] = "Developer"
+print("==========")
+print(participantes)
+
+
+
+jugador = {}
+
+jugador["nickname"] = "MikkyGG"
+jugador["score"] = 0
+
+print(jugador)
+
+jugador["score"] = 60
+print("Tu score actual es: ", jugador["nickname"] + " es " + str(jugador["score"]))
+
+
+# DICCIONARIO AVENGERS
+
+avengers = {
+
+	"Cap": {
+		"nombre": "Steve",
+		"apellido": "Rogers",
+		"avenger": "Capitan America",
+	},
+
+	"Ironman": {
+		"nombre": "Tony",
+		"apellido": "Stark",
+		"avenger": "Ironman",
+	},
+
+	"Hulk": {
+		"nombre": "Bruce",
+		"apellido": "Banner",
+		"avenger": "Hulk",
+	}
+
+}
+
+
+for username, avenger_info in avengers.items():
+	print("\n Username " + username)
+	fullname = avenger_info["nombre"] + " " + avenger_info["apellido"]
+	avenger_name = avenger_info["avenger"]
+
+	print("\t Nombre real " + fullname)
+	print("\t Nombre del avenger + avenger")
