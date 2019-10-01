73dcd34456a438c634491b6b0233f0c8483f78c4
Miguel Palma <mikky@MacBook-Air-de-Miguel.local>
2019-10-01 08:10:44 -0500

    Commit message

---
+++ Ejercicio_4.py
@@ -0,0 +1,15 @@
+"""
+
+Ejercicio de Python 4: VALIDACION DE EDAD
+Nombre: Miguel David Palma Lara
+Materia: Programacion Visual
+
+"""
+
+edad = int(input("Ingresa tu edad: "))
+
+if edad < 18:
+	print("Eres menor de edad y no puedes votar")
+
+else:
+	print("Eres mayor de edad y puedes votar")
